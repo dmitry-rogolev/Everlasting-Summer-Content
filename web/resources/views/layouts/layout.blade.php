ded532a7a8815a7c97fb753587ea1ff9b112c162
Dmitry <work.drogolev@internet.ru>
2022-05-26 19:43:06 +0700

    Изменение компонента Layout

--- web/resources/views/layouts/layout.blade.php
+++ web/resources/views/layouts/layout.blade.php
@@ -7,19 +7,13 @@
                 <x-body.header theme="{{ $theme }}" class="col-12 max-width-xl" />
                 <main class="col-12 max-width-xl">
                     <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12 p-0">
-                                <div class="container-fluid">
-                                    <div class="row flex-column">
-                                        <section class="col-12 mb-2 p-0">
-                                            <x-body.main.header header="{{ $header }}" referer="{{ $referer }}" />
-                                        </section>
-                                        <section class="col-12 p-0">
-                                            {{ $slot }}
-                                        </section>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="row flex-column">
+                            <section class="col-12 mb-2 p-0">
+                                <x-body.main.header header="{{ $header }}" referer="{{ $referer }}" />
+                            </section>
+                            <section class="col-12 p-0">
+                                {{ $slot }}
+                            </section>
                         </div>
                     </div>
                 </main>
