4a08d860dccba24e68aed7540c6e67ef11f6398a
Dmitry <work.drogolev@internet.ru>
2022-05-27 13:01:07 +0700

    Добавление шаблона general

---
+++ web/resources/views/general.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+    <x-slot:header>
+        {{ $header }}
+    </x-slot:header>
+    <x-slot:referer>
+        {{ $referer }}
+    </x-slot:referer>
+    <x-slot:theme>
+        {{ $theme }}
+    </x-slot:theme>
+
+    
+
+</x-layout>
