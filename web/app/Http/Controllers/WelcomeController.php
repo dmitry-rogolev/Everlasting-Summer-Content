4d60c9ec125106ad1552d9cd37b86309636bffa7
Dmitry <work.drogolev@internet.ru>
2022-05-27 12:54:08 +0700

    Добавление контроллера WelcomeController

---
+++ web/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class WelcomeController extends ContentController
+{
+    public function __construct()
+    {
+        $breadcrumbs = new Collection([
+            "Главная" => url("/"), 
+        ]);
+
+        parent::__construct(config("view.title"), "Главная", "", $breadcrumbs);
+    }
+
+    public function show(Request $request)
+    {
+        $this->theme($request);
+
+        return view("general", [
+            "title" => $this->title, 
+            "header" => $this->header, 
+            "referer" => $this->referer, 
+            "theme" => $this->theme, 
+        ]);
+    }
+}
