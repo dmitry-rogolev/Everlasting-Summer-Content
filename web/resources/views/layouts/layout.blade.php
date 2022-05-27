083f69d46fc15953d523aa192888abd99e867225
Dmitry <work.drogolev@internet.ru>
2022-05-27 16:38:44 +0700

    Изменение шаблонов layout и welcome

--- web/resources/views/layouts/layout.blade.php
+++ web/resources/views/layouts/layout.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html lang="ru">
     <x-head theme="{{ $theme }}" title="{{ $title }}" />
-    <body class="background height-100">
-        <div class="background-albugo height-100">
-            {{ $slot }}
-        </div>
-    </body>
+    {{ $slot }}
 </html>
--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -7,22 +7,26 @@
         {{ $theme }}
     </x-slot:theme>
 
-    <div class="container-fluid">
-        <div class="row flex-column align-items-center">
-            <x-body.header theme="{{ $theme }}" class="col-12 max-width-xl" />
-            <main class="col-12 max-width-xl">
-                <div class="container-fluid">
-                    <div class="row flex-column">
-                        <section class="col-12 mb-2 p-0">
-                            <x-body.main.header header="{{ $header }}" referer="{{ $referer }}" />
-                        </section>
-                        <section class="col-12 p-0">
-                            
-                        </section>
-                    </div>
+    <body class="background height-100">
+        <div class="background-albugo height-100">
+            <div class="container-fluid">
+                <div class="row flex-column align-items-center">
+                    <x-body.header theme="{{ $theme }}" class="col-12 max-width-xl" />
+                    <main class="col-12 max-width-xl">
+                        <div class="container-fluid">
+                            <div class="row flex-column">
+                                <section class="col-12 mb-2 p-0">
+                                    <x-body.main.header header="{{ $header }}" referer="{{ $referer }}" />
+                                </section>
+                                <section class="col-12 p-0">
+                                    
+                                </section>
+                            </div>
+                        </div>
+                    </main>
                 </div>
-            </main>
+            </div>
         </div>
-    </div>
-
+    </body>
+    
 </x-layout>
