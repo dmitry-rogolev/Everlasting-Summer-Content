7db376abbb2e749ba512207a2a947051e8b49743
Dmitry <work.drogolev@internet.ru>
2022-06-02 06:27:22 +0700

    Добавление компонента Element/Form/Custom/CustomSwitch

---
+++ web/resources/views/components/element/form/custom/switch.blade.php
@@ -0,0 +1,8 @@
+<x-element.form.custom.control class="custom-switch">
+    <input type="checkbox" class="custom-control-input" id="{{ $id }}" />
+    @if ($label)
+        <x-element.form.label class="custom-control-label" for="{{ $id }}">
+            {{ $label }}
+        </x-element.form.label>
+    @endif
+</x-element.form.custom.control>
