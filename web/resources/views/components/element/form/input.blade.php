93dcdfec8c4c6c80711b0263b89dda20dfe20bab
Dmitry <work.drogolev@internet.ru>
2022-06-02 05:18:40 +0700

    Добавление компонента Element/Form/Input

---
+++ web/resources/views/components/element/form/input.blade.php
@@ -0,0 +1,11 @@
+@if ($label) 
+    <x-element.form.label for="{{ $id }}">
+        {{ $label }}
+    </x-element.form.label>
+@endif
+<input {{ $attributes }} class="form-control {{ $class }}" id="{{ $id }}" @if ($small) aria-describedby="{{ $aria }}" @endif />
+@if ($small)
+    <x-element.form.small id="{{ $aria }}" class="form-text text-muted">
+        {{ $small }}
+    </x-element.form.small>
+@endif
