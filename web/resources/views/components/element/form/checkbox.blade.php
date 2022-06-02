130bd2546164448826f6e2434bfdd8845427e926
Dmitry <work.drogolev@internet.ru>
2022-06-02 10:10:37 +0700

    Добавление компонента Element/Form/Checkbox

---
+++ web/resources/views/components/element/form/checkbox.blade.php
@@ -0,0 +1,8 @@
+<div class="form-check">
+    <input type="checkbox" class="form-check-input {{ $class }}" id="{{ $id }}" {{ $attributes }} />
+    @if ($label) 
+        <label class="form-check-label" for="{{ $id }}">
+            {{ $label }}
+        </label>
+    @endif
+</div>
