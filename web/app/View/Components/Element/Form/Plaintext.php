adabe65a061f4e7d90c40525dc531811d34f5202
Dmitry <work.drogolev@internet.ru>
2022-06-02 09:58:45 +0700

    Добавление компонента Element/Form/Plaintext

---
+++ web/app/View/Components/Element/Form/Plaintext.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\View\Components\Element\Form;
+
+use App\View\Components\Component;
+use Illuminate\Support\Str;
+
+class Plaintext extends Component
+{
+    protected string $id;
+
+    protected string $aria;
+
+    protected string $label;
+
+    protected string $small;
+
+    protected string $class;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(?string $label = null, ?string $small = null, ?string $class = null)
+    {
+        parent::__construct();
+
+        $this->id = Str::random(10);
+        $this->aria = Str::random(10);
+        $this->label = $label ?? "";
+        $this->small = $small ?? "";
+        $this->class = $class ?? "";
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.element.form.plaintext', 
+        [
+            "theme" => $this->theme, 
+            "themes" => $this->themes, 
+            "inversion_themes" => $this->inversionThemes, 
+            "id" => $this->id, 
+            "aria" => $this->aria, 
+            "label" => $this->label, 
+            "small" => $this->small, 
+            "class" => $this->class, 
+        ]);
+    }
+}
--- web/resources/views/auth/register.blade.php
+++ web/resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
                         <main class="col-12 {{ 'bg-' . $theme }}">
                             <form method="POST" action="{{ route('register') }}">
                                 <x-element.form.group>
-                                    <x-element.form.custom.file label="label" />
+                                    <x-element.form.file label="label" />
                                 </x-element.form.group>
                             </form>
                         </main>
