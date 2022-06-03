3966fbc2de4413f81a502d1ddbdfb395f530adae
Dmitry <work.drogolev@internet.ru>
2022-06-03 10:22:55 +0700

    Добавление шаблона reset-password

--- web/resources/views/auth/reset-password.blade.php
+++ web/resources/views/auth/reset-password.blade.php
@@ -1,48 +1,49 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
-
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-body>
+        <x-element.background>
+            <x-element.flex flex="align-items-center justify-content-center vh-100">
+                <div class="col-xl-4 col-lg-5 col-md-7 col-sm-10 col-11 px-0 py-3">
+                    <x-element.flex flex="flex-column" class="{{ 'bg-' . $theme }} shadow-lg pb-3" style="border-radius: 20px;">
+                        <header class="col-12 px-0">
+                            <a 
+                                href="{{ url('/') }}"
+                                class="d-block p-3 text-center cursor-pointer text-decoration-none {{ 'text-' . $inversion_themes->get($theme) }}"
+                                style="border-top-left-radius: 20px; border-top-right-radius: 20px;"
+                                >
+                                <h2 class="mb-0">
+                                    {{ $header }}
+                                </h2>
+                            </a>
+                        </header>
+                        <main class="col-12 pt-3 text-{{ $inversion_themes->get($theme) }}">
+                            <x-auth.error />
+                            <form method="POST" action="{{ route('password.update') }}">
+                                <x-element.form.group>
+                                    @csrf
+                                </x-element.form.group>
+                                <x-element.form.group>
+                                    <x-element.form.input type="hidden" name="token" value="{{ $request->route('token') }}" />
+                                </x-element.form.group>
+                                <x-element.form.group>
+                                    <x-element.form.input type="email" class="form-control-lg bg-{{ $theme }} text-{{ $inversion_themes->get($theme) }}" name="email" label="Электронная почта" placeholder="Электронная почта" :value="old('email')" required autocomplete="off" accesskey="e" tabindex="1" />
+                                </x-element.form.group>
+                                <x-element.form.group>
+                                    <x-element.form.input type="password" class="form-control-lg bg-{{ $theme }} text-{{ $inversion_themes->get($theme) }}" name="password" label="Новый пароль" placeholder="Новый пароль" required autocomplete="off" accesskey="p" tabindex="2" />
+                                </x-element.form.group>
+                                <x-element.form.group>
+                                    <x-element.form.input type="password" class="form-control-lg bg-{{ $theme }} text-{{ $inversion_themes->get($theme) }}" name="password_confirmation" label="Подтверждение пароля" placeholder="Подтверждение пароля" required autocomplete="off" accesskey="c" tabindex="3" />
+                                </x-element.form.group>
+                                <x-element.form.group class="text-center mt-4">
+                                    <x-element.form.button type="submit" class="btn-lg btn-{{ $inversion_themes->get($theme) }}" tabindex="5">
+                                        Сбросить пароль
+                                    </x-element.form.button>
+                                </x-element.form.group>
+                            </form>
+                        </main>
+                    </x-element.flex>
+                </div>
+            </x-element.flex>
+        </x-element.background>
+    </x-body>
+</x-layout>
