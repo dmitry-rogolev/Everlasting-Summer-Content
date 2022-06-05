85d091a29e2597f85bd7b7c65941783d4d7f67d2
Dmitry <work.drogolev@internet.ru>
2022-06-05 18:32:49 +0700

    Изменение компонента Element/Form/Custom/Checkbox

--- web/resources/views/components/element/form/custom/checkbox.blade.php
+++ web/resources/views/components/element/form/custom/checkbox.blade.php
@@ -1,8 +1,11 @@
-<x-element.form.custom.control class="custom-checkbox">
-    <input type="checkbox" class="custom-control-input {{ $class }}" id="{{ $id }}" {{ $attributes }} />
-    @if ($label) 
-        <label class="custom-control-label" for="{{ $id }}">
-            {!! $label !!}
-        </label>
-    @endif
-</x-element.form.custom.control>
+<x-element.flex>
+    <x-element.form.custom.control class="custom-checkbox cursor-pointer">
+        <input type="checkbox" class="custom-control-input {{ $class }}" id="{{ $id }}" {{ $attributes }} />
+        @if ($label) 
+            <label class="custom-control-label" for="{{ $id }}">
+                {!! $label !!}
+            </label>
+        @endif
+    </x-element.form.custom.control>
+</x-element.flex>
+    
